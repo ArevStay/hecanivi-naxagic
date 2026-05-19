43997ebdc4a8ec5cc059919ec6b0a2e7c415a52c
Student <arevik.maloyan@tumo.org>
2026-05-19 19:50:25 +0400

    skz

---
+++ src/practice.js
@@ -0,0 +1,35 @@
+// // const name1 = "Stray kids"
+// let debut = 2018
+// let members = ["Bang Chan", "Lee Know", "Changbin", "Hyunjin", "Han", "Felix", "Seungmin", "I.N"]
+
+// console.log(debut);
+// // console.log(name);
+// console.log(members);
+
+// let Coord = 0.621
+
+// function milesToKm(miles) {
+//     return miles * Coord
+// }
+
+// console.log(milesToKm(3));
+
+// let name = "Stray kids"
+// let craft = " that get fans singing along."
+// function sentences(name, craft) {
+   
+//     return name+" always deliver high-energy performances"+craft
+   
+// }
+// console.log(sentences(name,craft));
+
+
+
+const IssPosition = {
+    latitude: 49.2167,
+    longitude: -122.9167
+   
+   
+}
+
+console.log(IssPosition.latitude, IssPosition.longitude);
